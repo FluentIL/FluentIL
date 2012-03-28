c495faf6ea9beb858acb54380cc6128968557c0e
elemarjr <elemarjr@gmail.com>
2012-03-28 14:26:51 -0300

    Basic While/Loop Fluent Fluent Interface

---
+++ src/FluentIL/Emitters/Specialized/DynamicMethodBody.WhileLoop.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+// ReSharper disable CheckNamespace
+
+namespace FluentIL.Emitters
+// ReSharper restore CheckNamespace
+{
+    partial class DynamicMethodBody
+    {
+        public DynamicMethodBody While(string condition)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DynamicMethodBody Until(string condition)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DynamicMethodBody While(
+            string condition,
+            Action<DynamicMethodBody> @do
+            )
+        {
+            While(condition);
+                @do(this);
+            Loop();
+            return this;
+        }
+
+        public DynamicMethodBody Until(
+            string condition,
+            Action<DynamicMethodBody> @do
+            )
+        {
+            Until(condition);
+                @do(this);
+            Loop();
+            return this;
+        }
+
+        public DynamicMethodBody Loop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
