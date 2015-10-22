c009aa926d5283cb511678b817d31713393cfa26
Elemar Rodrigues Severo Junior <elemarjr@gmail.com>
2015-10-22 14:59:54 -0200

    ImageProcessing demo

---
+++ demo/ImageProcessing/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using FluentIL;
+
+namespace ImageProcessing
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new AppForm());
+        }
+    }
+}
