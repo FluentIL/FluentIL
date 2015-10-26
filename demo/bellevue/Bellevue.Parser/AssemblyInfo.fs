e6548142c6beea5c44d26eea7a3c483181d57a82
Elemar Rodrigues Severo Junior <elemarjr@gmail.com>
2015-10-26 17:11:15 -0200

    limiting comments

---
+++ demo/bellevue/Bellevue.Parser/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace Bellevue.Parser.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("Bellevue.Parser")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("Bellevue.Parser")>]
+[<assembly: AssemblyCopyright("Copyright ©  2015")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("8b97b38d-96ae-489a-8dfc-0ae4d6452b68")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ demo/bellevue/Bellevue.Parser/parser.fs
@@ -0,0 +1,48 @@
+﻿
+namespace Bellevue.Parser
+
+module private Util =
+    let toString chars =
+       System.String(chars |> Array.ofList)
+
+    let (|StartsWith|_|) prefix list =
+        let rec loop = function
+            | [], rest -> Some(rest)
+            | p::prefix, r::rest when p = r -> loop (prefix, rest)
+            | _ -> None
+        loop (prefix, list)
+
+    let rec parseBracketedBody closing acc = function
+        | StartsWith closing (rest) -> Some(List.rev acc, rest)
+        | c::chars -> parseBracketedBody closing (c::acc) chars
+        | _ -> None
+
+    let (|Bracketed|_|) opening closing = function
+        | StartsWith opening chars -> parseBracketedBody closing [] chars
+        | _ -> None
+  
+    let (|Delimited|_|) delim = (|Bracketed|_|) delim delim
+
+    let rec parse acc chars = seq {
+        let emitLiteral() = seq {
+            if acc <> [] then 
+                yield acc |> List.rev |> toString
+        }
+
+        match chars with 
+        | Bracketed ['@'; '*' ] ['*'; '@' ] (body, chars) ->
+            yield! emitLiteral() 
+            yield! parse [] chars
+        | h::t -> 
+            yield! parse (h::acc) t
+        | [] ->
+            yield! emitLiteral()
+    }
+
+module TextParser = 
+    open Util
+
+    let Parse input = 
+        input 
+        |> List.ofSeq
+        |> parse [] 
